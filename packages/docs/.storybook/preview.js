bb73ca960b49ffdd4333641758dbcfae4b3a2093
Sullivan Dcesaris <sullivandcesaris.dev@gmail.com>
2023-05-27 15:26:52 -0300

    STORYBOOK: changes config

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,14 +1,19 @@
 import { themes } from '@storybook/theming'
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
     },
+    docs: {
+      theme: themes.dark
+    }
   },
-  docs: {
-    theme: themes.dark,
-  }
-}
+};
+
+export default preview;
