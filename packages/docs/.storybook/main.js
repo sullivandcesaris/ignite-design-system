82a583d70bd3cd166d51d454ee5ec731ad3781f8
Sullivan Dcesaris <sullivandcesaris.dev@gmail.com>
2023-05-27 15:54:54 -0300

    DEPLOY: changes config

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,20 +1,20 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/pages/**/*.stories.mdx",
     "../src/stories/**/*.stories.tsx"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite"
   },
-  "features": {
-    "storyStoreV7": true
+  features: {
+    storyStoreV7: true
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
